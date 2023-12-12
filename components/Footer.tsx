a3b69b69421ec7f25915dbc9cbd98b639e124c95
inwonakng <inwon.kang04@gmail.com>
2023-12-12 05:32:53 -0500

    footer link updated to my homepage

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link
-            href="https://github.com/timlrx/tailwind-nextjs-starter-blog"
+            href="https://github.com/inwonakg/homepage"
             className="text-gray-500 dark:text-gray-400"
           >
             Tailwind Nextjs Theme
