ca7628740d43e1e27113fa78d5f598a9af7f4c3e
inwonakng <inwon.kang04@gmail.com>
2023-12-12 05:14:20 -0500

    home linke color fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,16 +22,18 @@ const Header = () => {
   return (
     <header>
       <div className="flex items-center justify-between py-10">
-        <div>
-          <Link href="/" aria-label={siteMetadata.headerTitle}>
-            <div className="flex items-center justify-between">
+        <div className="text-primary">
+            <div className="flex items-center justify-between ">
               {typeof siteMetadata.headerTitle === 'string' ? (
-                <div className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
+                <Link href="/" aria-label={siteMetadata.headerTitle} className="text-gray-900 dark:text-gray-100">
+                  <div className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
+                  </Link>
               ) : (
+                <Link href="/" aria-label={siteMetadata.headerTitle} className="text-gray-900 dark:text-gray-100" >
                 siteMetadata.headerTitle
+                  </Link>
               )}
             </div>
-          </Link>
         </div>
         <div className="hidden sm:block">
           <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
